5cf0ffee23d1b900861e96abbf9d44729dcd8323
deep joshi <deep.j@cypheritsolutions.com>
2024-09-30 18:50:05 +0530

    Resolved one issue regarding inject

--- app/src/main/java/com/example/lootlearn/presentation/screens/authChoices/AuthRepository.kt
+++ app/src/main/java/com/example/lootlearn/presentation/screens/authChoices/AuthRepository.kt
@@ -4,8 +4,9 @@ import com.example.lootlearn.model.FbLoginModel
 import com.example.lootlearn.requestModel.FbLoginRequestModel
 import com.example.lootlearn.webservices.ApiClient
 import retrofit2.Response
+import javax.inject.Inject
 
-class AuthRepository {
+class AuthRepository @Inject constructor(){
     suspend fun fbLogin(requestModel: FbLoginRequestModel): Response<FbLoginModel> {
         return ApiClient.api.fbLogin(requestModel)
     }
